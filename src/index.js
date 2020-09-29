207e1f2b5f2ffcdaf63c824b7e6803da1384cec4
Alexandr Khokhanov <hohanov@gmail.com>
2020-09-29 08:13:53 +0300

    feat: add 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (Array.isArray(array) && array.length !== 0) return Math.min.apply(Math, array);
+    return 0;
+  }
+  
+  exports.max = function max (array) {
+    if (Array.isArray(array) && array.length !== 0) return Math.max.apply(Math, array);
+    return 0;
+  }
+  
+  exports.avg = function avg (array) {
+      if (Array.isArray(array) && array.length !== 0) {
+        sum = 0;
+        array.forEach((element) => {
+          sum += element;
+          });
+        return (sum/array.length);
+      };
+    return 0;
+  }
+  
+  
